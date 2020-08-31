6d61f1be605984d9a74166ef87672ba604b08ba7
prasha1596 <Prasha1596@gmail.com>
2020-08-31 13:24:41 +0530

    Singly Linked list

---
+++ DSA/Lists/singlyLinkedList/EmployeeNode.java
@@ -0,0 +1,33 @@
+package singlyLinkedList;
+
+public class EmployeeNode {
+	private Employee employee;
+	private EmployeeNode next;
+	
+	EmployeeNode(Employee employee) {
+		this.employee=employee;
+	}
+
+	public Employee getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+
+	public EmployeeNode getNext() {
+		return next;
+	}
+
+	public void setNext(EmployeeNode next) {
+		this.next = next;
+	}
+
+	@Override
+	public String toString() {
+		//return "EmployeeNode [employee=" + employee + ", next=" + next + "]";
+		return employee.toString();
+	}
+	
+}
